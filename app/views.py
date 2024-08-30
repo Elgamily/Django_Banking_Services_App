e80dcab28013ee5f1f54089880d1bdbd186ea509
Hassan <hassanelgamily@gmail.com>
2024-08-30 12:51:54 +0200

    update app/views.py

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import logout
 from django.views.generic import CreateView, TemplateView, View, ListView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
+from django.db.models import Sum, Max
 from .models import History
 from .forms import CreateUserForm
 import requests
@@ -14,60 +15,100 @@ def logout_view(request):
     return redirect('login') 
 
 def getBalance(user):
-    pass # this line can be deleted 
-    '''
-    Write a function that finds the user's balance and returns it with the float data type. 
-    To calculate the balance, calculate the sum of all user's deposits and the sum of all withdrawals.
-    Then subtract the withdrawal amount from the deposit amount and return the result.
-    '''
+    # '''
+    # Write a function that finds the user's balance and returns it with the float data type. 
+    # To calculate the balance, calculate the sum of all user's deposits and the sum of all withdrawals.
+    # Then subtract the withdrawal amount from the deposit amount and return the result.
+    # '''
+
+    total_deposits = History.objects.filter(user=user, status='success', type='deposit').aggregate(total=Sum('amount'))['total'] or 0.0
+    
+    total_withdrawals = History.objects.filter(user=user, status='success', type='withdraw').aggregate(total=Sum('amount'))['total'] or 0.0
+    
+    balance = float(total_deposits) - abs(float(total_withdrawals))
+
+    return float(balance)
+
 def getCurrencyParams():
-    pass # this line can be deleted 
-    '''
-    Write a function that makes a GET request to the following address 
-    https://fake-api.apps.berlintech.ai/api/currency_exchange
+    # '''
+    # Write a function that makes a GET request to the following address 
+    # https://fake-api.apps.berlintech.ai/api/currency_exchange
 
-    if the response code is 200 return a list of two values:
-    - a dictionary of data that came from the server
-    - a list of strings based on the received data 
-    mask to form the string f'{currency} ({rate})'.
-    example string: 'USD (1.15)'
+    # if the response code is 200 return a list of two values:
+    # - a dictionary of data that came from the server
+    # - a list of strings based on the received data 
+    # mask to form the string f'{currency} ({rate})'.
+    # example string: 'USD (1.15)'
 
-    if the server response code is not 200 you should 
-    return the list [None, None]
-    '''
+    # if the server response code is not 200 you should 
+    # return the list [None, None]
+    # '''
 
+    url = 'https://fake-api.apps.berlintech.ai/api/currency_exchange'
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            formatted_list = [(currency, f"{currency} ({rate})") for currency, rate in data.items()]
+            
+            return [data, formatted_list]
+        else:
+            return [None, None]
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return [None, None]
 
 class CreateUserView(CreateView):
-    '''
-    Finalize this class. It should create a new user.
-    The model should be the User model
-    The CreateUserForm model should be used as a form.
-    The file create_account.html should be used as a template.
-    If the account is successfully created, it should redirect to the page with the name login
-    '''
+    # '''
+    # Finalize this class. It should create a new user.
+    # The model should be the User model
+    # The CreateUserForm model should be used as a form.
+    # The file create_account.html should be used as a template.
+    # If the account is successfully created, it should redirect to the page with the name login
+    # '''
+
+    model = User
+    form_class = CreateUserForm
+    template_name = 'app/create_account.html'
+    success_url = reverse_lazy('login')
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        '''
-        If the user is authenticated, then add the 'username' key with the value of username to the context.
-        '''
+        # '''
+        # If the user is authenticated, then add the 'username' key with the value of username to the context.
+        # '''
+
+        if self.request.user.is_authenticated:
+            context['username'] = self.request.user.username
+        
         return context
 
 class CustomLoginView(LoginView):
-    '''
-    Modify this class. 
-    specify the login.html file as the template
-    if authentication is positive, add redirect to main_menu page
-    '''
+    # '''
+    # Modify this class. 
+    # specify the login.html file as the template
+    # if authentication is positive, add redirect to main_menu page
+    # '''
+    template_name = 'app/login.html'
+    success_url = reverse_lazy('main_menu')
+    # redirect_authenticated_user = True
 
     def get_success_url(self):
         return self.success_url
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        '''
-        If the user is authenticated, then add the 'username' key with the value of username to the context.
-        '''
+        # '''
+        # If the user is authenticated, then add the 'username' key with the value of username to the context.
+        # '''
+        
+        if self.request.user.is_authenticated:
+            context['username'] = self.request.user.username
+
         return context
 
 class MainMenuView(LoginRequiredMixin, TemplateView):
@@ -75,41 +116,101 @@ class MainMenuView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        '''
-        If the user is authenticated, then add the 'username' key with the value of username to the context.
-        '''
+        # '''
+        # If the user is authenticated, then add the 'username' key with the value of username to the context.
+        # '''
+        if self.request.user.is_authenticated:
+            context['username'] = self.request.user.username
+
         return context
 
 class BalanceOperationsView(LoginRequiredMixin, View):
     template_name = 'app/operations.html'
     
     def get(self, request):
-        pass # this line can be deleted 
-        '''
-        This method should return the page given in template_name with a context.
+        # '''
+        # This method should return the page given in template_name with a context.
 
-        Context is a dictionary with balance and username keys.
-        The balance key contains the result of the getBalance function
-        username contains the username of the user.
-        '''
+        # Context is a dictionary with balance and username keys.
+        # The balance key contains the result of the getBalance function
+        # username contains the username of the user.
+        # '''
+
+        balance = getBalance(request.user)
+        context = {
+            'balance': balance,
+            'username': request.user.username
+        }
+        return render(request, self.template_name, context)
 
     def post(self, request):
-        pass # this line can be deleted 
-        '''
-        This method should process a balance transaction.
-        For this purpose it is necessary to add an entry to the History model. 
+        # '''
+        # This method should process a balance transaction.
+        # For this purpose it is necessary to add an entry to the History model. 
         
-        status - if the amount on the account is not enough when attempting to withdraw funds, the status is failure, otherwise withdraw
-        amount - amount of operation, obtained from the form
-        type - type of operation (withdraw/deposit), the value is obtained from the form.
-        user - object of the current user
+        # status - if the amount on the account is not enough when attempting to withdraw funds, 
+        # the status is failure, otherwise withdraw
+        # amount - amount of operation, obtained from the form
+        # type - type of operation (withdraw/deposit), the value is obtained from the form.
+        # user - object of the current user
 
-        This method should return the page given in template_name with a context.
+        # This method should return the page given in template_name with a context.
 
-        Context is a dictionary with balance and username keys.
-        The balance key contains the result of the getBalance function (after account update)
-        username contains the username of the user.
-        '''
+        # Context is a dictionary with balance and username keys.
+        # The balance key contains the result of the getBalance function (after account update)
+        # username contains the username of the user.
+        # '''
+
+        amount = request.POST.get('amount')
+        type = request.POST.get('operation')
+
+        if amount is None or type not in ['withdraw', 'deposit']:
+            return self.get(request)
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            return self.get(request)
+
+        current_balance = getBalance(request.user)
+
+        if type == 'withdraw':
+            if amount > current_balance:
+                new_balance = current_balance
+                status = 'failure'
+                History.objects.create(
+                    user=request.user,
+                    amount=amount,
+                    type=type,
+                    status=status
+                )
+            else:
+                new_balance = current_balance - amount
+                status = 'success'
+                History.objects.create(
+                    user=request.user,
+                    amount=-amount,
+                    type=type,
+                    status=status
+                )
+        elif type == 'deposit':
+            new_balance = current_balance + amount
+            status = 'success'
+            History.objects.create(
+                user=request.user,
+                amount=amount,
+                type=type,
+                status=status
+            )
+        else:
+            return self.get(request)
+
+        context = {
+            'balance': new_balance,
+            'username': request.user.username
+        }
+
+        return render(request, self.template_name, context)
 
 class ViewTransactionHistoryView(LoginRequiredMixin, ListView):
     model = History
@@ -118,15 +219,20 @@ class ViewTransactionHistoryView(LoginRequiredMixin, ListView):
     ordering = ['-datetime']
 
     def get_queryset(self):
-        '''
-        This method should return the entire transaction history of the current user
-        '''
+        # '''
+        # This method should return the entire transaction history of the current user
+        # '''
+        transactions = History.objects.filter(user=self.request.user)
+
+        return transactions
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        '''
-        Add the 'username' key with the value of username to the context.
-        '''
+        # '''
+        # Add the 'username' key with the value of username to the context.
+        # '''
+
+        context['username'] = self.request.user.username
         return context
 
 class CurrencyExchangeView(LoginRequiredMixin, View):
@@ -135,22 +241,67 @@ class CurrencyExchangeView(LoginRequiredMixin, View):
 
     def get(self, request):
         _, currency_choices = getCurrencyParams()
-        '''
-        Generate a context variable with all values from empty_context and the converted values of currency_choices and username
-        currency_choices contains the value of the currency_choices variable
-        username contains the name of the current user
-        '''
+        # '''
+        # Generate a context variable with all values from empty_context and the converted values of currency_choices and username
+        # currency_choices contains the value of the currency_choices variable
+        # username contains the name of the current user
+        # '''
+
+        username = request.user.username
+        
+        context = {
+            'currency_choices': currency_choices,       
+            'amount': None,                             
+            'currency': None,                           
+            'exchanged_amount': None,                   
+            'username': username                        
+        }
+
         return render(request, self.template_name, context)
 
     def post(self, request):
         data, currency_choices = getCurrencyParams()
-        '''
-            Improve this method:
-            1) add the process of forming the variable amount.
-            If the amount value from the form is converted to float type, then write the amount value from the form converted to float to the amount variable. Otherwise, write None.
-            2) add a currency variable that contains the currency value from the form.
-            3) if the variables data or amount contain None, return page with empty context (empty_context). Otherwise, perform the following steps
-            4) generate the exchange_rate variable by calculating the corresponding value from the data variable
-            5) generate the exchanged_amount variable, which contains the converted currency to two decimal places.
-            6) form a context from the previously created variables and return a template with it.
-        '''
+        # '''
+        #     Improve this method:
+        #     1) add the process of forming the variable amount.
+        #     If the amount value from the form is converted to float type, then write the amount value from 
+        #     the form converted to float to the amount variable. Otherwise, write None.
+        #     2) add a currency variable that contains the currency value from the form.
+        #     3) if the variables data or amount contain None, return page with empty context (empty_context). 
+        #     Otherwise, perform the following steps
+        #     4) generate the exchange_rate variable by calculating the corresponding value from 
+        #     the data variable
+        #     5) generate the exchanged_amount variable, 
+        #     which contains the converted currency to two decimal places.
+        #     6) form a context from the previously created variables and return a template with it.
+        # '''
+
+        try:
+            amount = float(request.POST.get('amount'))
+        except (TypeError, ValueError):
+            amount = None
+
+        currency = request.POST.get('currency')
+
+        empty_context = {
+            'currency_choices': currency_choices,
+            'amount': None,
+            'currency': None,
+            'exchanged_amount': None
+        }
+
+        if data is None or amount is None or currency is None:
+            return render(request, self.template_name, empty_context)
+        
+        exchange_rate = data.get(currency)
+
+        exchanged_amount = round(amount * exchange_rate, 2) if exchange_rate else None
+
+        context = {
+            'currency_choices': currency_choices,
+            'amount': amount,
+            'currency': currency,
+            'exchanged_amount': exchanged_amount,
+        }
+
+        return render(request, self.template_name, context)
